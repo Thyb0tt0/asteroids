e6eea9cd38faf2fa3e72f4dea992f7955a46e11f
Thyb0tt0 <danielthybo.rasmussen@gmail.com>
2024-08-21 16:08:35 +0200

    added feature that lets player move forward and backwards

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 import circleshape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_SPEED
 from constants import PLAYER_TURN_SPEED
 
 class Player(circleshape.CircleShape):
@@ -29,3 +29,11 @@ class Player(circleshape.CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
