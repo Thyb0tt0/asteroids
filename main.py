acd05ef5589145d315b46c85bfac7fe0d3047418
Thyb0tt0 <danielthybo.rasmussen@gmail.com>
2024-08-21 15:04:37 +0200

    added player as a triangle

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import pygame 
+import pygame
 from constants import *
+from player import *
 
 def main():
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, 0)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
